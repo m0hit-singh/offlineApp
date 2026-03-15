1d0c8e66f2ecd413346bd8ca1feec2811c168a1c
Mohitsingh <mohit.025st@gmail.com>
2026-03-15 22:59:48 +0530

    Updates

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = "cache-v1";
+
+const ASSETS = ["index.html"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => {
+      return c.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheNames) return caches.delete(cache);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const resClone = res.clone();
+
+        caches.open(CACHE).then((cache) => cache.put(e.request, resClone));
+        return res;
+      })
+      .catch((err) => {
+        return caches.match(e.request).then((res) => res);
+      })
+  );
+});
